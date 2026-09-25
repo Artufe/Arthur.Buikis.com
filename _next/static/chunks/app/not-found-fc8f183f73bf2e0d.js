502e69761ecdade5af14df02f3ee541dca073813
Artufe <Artufe@users.noreply.github.com>
2026-09-25 17:11:01 +0000

    deploy: a49d2c49a01e0c691bbf91faa243b812218e9743

--- _next/static/chunks/app/not-found-fc8f183f73bf2e0d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{1522:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9959,23))}},e=>{e.O(0,[959,796,569,358],()=>e(e.s=1522)),_N_E=e.O()}]);
